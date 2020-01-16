33b96f50df458721c7ddec7f7a84268694b19b2e
hc.lx <hc.lx@alibaba-inc.com>
2020-01-17 02:33:25 +0800

    mod

--- main.py
+++ main.py
@@ -42,14 +42,14 @@ sess.run(init)
 
 def pareto_step(weights_list, out_gradients_list):
     model_gradients = out_gradients_list
-    M1 = model_gradients*np.transpose(model_gradients)
+    M1 = np.matmul(model_gradients,np.transpose(model_gradients))
     e = np.mat(np.ones(np.shape(weights_list)))
     M = np.hstack((M1,np.transpose(e)))
     mid = np.hstack((e,np.mat(np.zeros((1,1)))))
     M = np.vstack((M,mid))
     z = np.mat(np.zeros(np.shape(weights_list)))
     nid = np.hstack((z,np.mat(np.ones((1,1)))))
-    w = M*np.linalg.inv(M*np.transpose(M))*np.transpose(nid)
+    w = np.matmul(np.matmul(M,np.linalg.inv(np.matmul(M,np.transpose(M)))),np.transpose(nid))
     if len(w)>1:
         w = np.transpose(w)
         w = w[0,0:np.shape(w)[1]]
@@ -63,6 +63,7 @@ def pareto_step(weights_list, out_gradients_list):
         w = np.where(nid - theta>0.0, nid - theta, 0)
     return w
 
+
 w_a = 0.5
 w_b = 0.5
 for step in xrange(0, 50):
@@ -73,3 +74,4 @@ for step in xrange(0, 50):
     mid = pareto_step(weights,paras)
     w_a, w_b = mid[0,0], mid[0,1]
     print w_a, w_b, step, sess.run(W), sess.run(b)
+    #print alpha, 1.0-alpha
